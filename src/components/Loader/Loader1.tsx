3ec405e34f1b18450d61d989399990ac95191a46
`mayankjoshi` <`mayankjoshi9871@gmail.com`>
2024-08-14 13:40:50 +0530

    correcting loader image path

--- src/components/Loader/Loader1.tsx
+++ src/components/Loader/Loader1.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Loader = () => {
   return (
     <div className='grow shrink flex justify-center items-center aspect-square h-full'>
-      <img src="/Images/Loader1.gif" alt="" className='h-full aspect-square animate-[rotate180_1s_linear_infinite]'/>
+      <img src="/images/Loader1.gif" alt="" className='h-full aspect-square animate-[rotate180_1s_linear_infinite]'/>
     </div>
   )
 }
--- src/components/Loader/Loader2.tsx
+++ src/components/Loader/Loader2.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Loader = () => {
   return (
     <div className='grow flex justify-center items-center'>
-      <img src="/Images/Loader2.gif" alt="" className='w-24 h-24'/>
+      <img src="/images/Loader2.gif" alt="" className='w-24 h-24'/>
     </div>
   )
 }
